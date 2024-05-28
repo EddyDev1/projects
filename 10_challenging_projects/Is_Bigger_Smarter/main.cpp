58e702f475bf90f82b9834faa6888c3e9d51505b
Edward Asante <edwardasante@my.unt.edu>
2024-05-28 12:34:21 -0500

    updated solution for old solution check previous commit

--- 10_challenging_projects/Is_Bigger_Smarter/main.cpp
+++ 10_challenging_projects/Is_Bigger_Smarter/main.cpp
@@ -13,53 +13,35 @@
 using std::cout;
 using std::cin;
 
-struct comp
-{
-    bool operator() (const int& lhs, const int& rhs) const
-    { return lhs > rhs; }
-};
-
 
 int main(int argc, char* argv[])
 {
-    std::map<int, std::priority_queue<int, std::vector<int>, std::greater<int>>, comp> mp;
-    std::unordered_map<int, int> nline;
-    
-    auto run_case = [&]() -> void
+    std::map<int, std::array<int, 3>, std::greater<int>> mp;
+
+    auto solve_case = [&]() -> void
     {
       auto it = mp.begin();
       int line, kg;
 
-      std::array<int, 3> prev = { it->second.top(), nline[it->first], it->first };
+      std::array<int, 3> prev = { it->second[0], it->second[1], it->second[2] };
       cout << prev[2] << " " << prev[0] << '\n';
       ++it;
 
       for (; it!= mp.end(); ++it)
       {
-        line = nline[it->first];
-        kg = it->second.top();
+        line = it->second[1];
+        kg = it->second[0];
 
         if (line > prev[1] && kg > prev[0])
         {
-          prev = { it->second.top(), line, it->first };
+          prev = { kg, line, it->second[2] };
           cout << prev[2] << " " << kg << '\n';
-        }
-        else if(line > prev[1] && it->second.size() > 1)
-        {
-          while(!it->second.empty() && it->second.top() < prev[0])
-            it->second.pop();
-
-          if (!it->second.empty())
-          {
-            prev = { it->second.top(), line, it->first };
-            cout << prev[2] << " " << kg << '\n';
-          }
         }
       }
 
       auto last_try = mp.find(prev[2]);
       ++last_try;
-      kg = last_try->second.top();
+      kg = last_try->second[0];
 
       if (kg > prev[0])
         cout << last_try->first << " " << kg << '\n';
@@ -74,16 +56,17 @@ int main(int argc, char* argv[])
       std::stringstream ss(data);
       ss >> kg >> iq;
       
-      mp[iq].push(kg);
-
-      if (nline.count(iq))
-        n++;
+      if (mp.count(iq))
+      {
+        mp[iq][0] = std::min(mp[iq][0], kg);
+        mp[iq][1] = mp[iq][0] == kg ? n++ : mp[iq][1];
+      }
       else
-        nline[iq] = n++;
+        mp[iq] = { kg, n++, iq };
     }
 
     ifs.close();
-    run_case();
+    solve_case();
         
     return 0;
 }
